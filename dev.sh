f719ca7c66a61235cca7157abfbb55d3ed8d4bea
Alix Chagué <33317799+alix-tz@users.noreply.github.com>
2022-02-25 01:43:36 -0500

    Toying with Github Action (#74)
    * adding a deployment script for local tests
    * compile i18n file before deployment
    * charter.yml is now included in deploy.yml
    * testing auto compile by making a very little modification in nav bar
    * [Automatic] Update the charter
    * adding missing commit for i18n files
    * [Automatic] Update language files
    * fix typo
    
    Fix #73 
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ dev.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Purpose: compile i18n files and run
+#           a local server for test in
+#           browser.
+# --------------------------------------
+
+if [[ "$OSTYPE" == msys ]]; then
+    echo 'Compile language files'
+    cd .\assets\vanilla-i18n && py compile.py 
+
+    echo 'Start local server'
+    echo 'Open http://0.0.0.0:8000/ or localhost:8000/ 
+            once the server has started'
+    py -m http.server
+
+else
+    echo 'Compile language files'
+    cd ./assets/vanilla-i18n/ && python3 compile.py 
+
+    echo 'Start local server'
+    echo 'Open http://0.0.0.0:8000/ or localhost:8000/ 
+            once the server has started'
+    python3 -m http.server
+fi
