c57b39c9537d64ecd8b762851bcf29c3eafd4ef4
tboenig <matthiasboenig@gmx.net>
2023-12-01 18:08:49 +0100

    Update form.js

--- static/js/form.js
+++ static/js/form.js
@@ -338,8 +338,20 @@
       return encodeURIComponent(`${output.innerText}`);
     }
 
+    const defaultBranch = data.default_branch;
+        fetch(`${(data.repoLink)}`)
+          .then(response => response.json())
+          .then(data => {
+            console.log(defaultBranch);
+    });
+
+
+    
+
+
+
     //alert("HELLO");
-    link.href = `${(data.repoLink)}/new/main?filename=METADATA.yml&value=${getOutputMetadataText()}`;
+    link.href = `${(data.repoLink)}/new/${defaultBranch}?filename=METADATA.yml&value=${getOutputMetadataText()}`;
     createIssueLink.href = `https://github.com/HTR-United/htr-united/issues/new?title=Adding%20dataset%20${(data.repoName)}&body=${getOutputIssueText()}`;
     
 
