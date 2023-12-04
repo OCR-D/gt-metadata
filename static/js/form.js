b7ae0da060763e6ad16e984fbc55add1fe8f5372
tboenig <matthiasboenig@gmx.net>
2023-12-04 14:24:21 +0100

    Update form.js

--- static/js/form.js
+++ static/js/form.js
@@ -338,17 +338,17 @@
       return encodeURIComponent(`${output.innerText}`);
     }
 
-    let data3;  {
-      const url = document.getElementById("data.repoLink").value;
-      const response = fetch(`https://api.github.com/repos/${url.split("/").slice(-2).join("/")}`);
-      const data2 = response.json();
-      data2.then(response => response.json())
-        .then(result => {
-          data = result;
-          // Code, der auf die Antwort zugreift
-        });
+    async function getRepoDefaultBranch() {
+      const url = "https://api.github.com/repos/<OWNER>/<REPO>";
+      const repoUrlElement = document.getElementById("data.repoLink");
+      if (repoUrlElement === null) {
+        console.error("Das Element wurde nicht gefunden.");
+        return;
+      }
+      const response = await fetch(`https://api.github.com/repos/${repoUrlElement.value.split("/").slice(-2).join("/")}`);
+      const data = await response.json();
+      return data.default_branch;
     }
-    
     
 
     
@@ -357,7 +357,7 @@
 
 
     //alert("HELLO");
-    link.href = `${(data3.repoLink)}/new/${data3.default_branch}?filename=METADATA.yml&value=${getOutputMetadataText()}`;
+    link.href = `${(data.repoLink)}/new/main?filename=METADATA.yml&value=${getOutputMetadataText()}`;
     createIssueLink.href = `https://github.com/HTR-United/htr-united/issues/new?title=Adding%20dataset%20${(data.repoName)}&body=${getOutputIssueText()}`;
     
 
